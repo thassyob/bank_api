8a450c33b272538ba0f3204b4b05531f5b7a1007
Thassyo Barbosa <thassyob@gmail.com>
2020-08-13 01:20:49 -0300

    Implement devise and authenticate only withdraw and transfer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_011148) do
+ActiveRecord::Schema.define(version: 2020_08_13_033650) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -22,9 +22,16 @@ ActiveRecord::Schema.define(version: 2020_07_31_011148) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
